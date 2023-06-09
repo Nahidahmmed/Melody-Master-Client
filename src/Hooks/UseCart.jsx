1e4ccc587f40f0407f36c0c07464839f2a131ae2
Nahidahmmed <nahidahmmed411@gmail.com>
2023-06-09 21:00:04 +0600

    query hook

--- src/Hooks/UseCart.jsx
+++ src/Hooks/UseCart.jsx
@@ -5,15 +5,15 @@ const UseCart = () => {
 
     const { user } = useContext(AuthContext);
 
-    const { isLoading, data: cart = [] } = useQuery({
-        queryKey: ['cart', user?.email],
+    const { refetch, data: cart = [] } = useQuery({
+        queryKey: ['carts', user?.email],
         queryFn: async () => {
             const response = await fetch(`http://localhost:5000/carts?email=${user?.email}`)
             return response.json();
         },
     })
 
-    return[cart, isLoading]
+    return[cart, refetch]
 };
 
 export default UseCart;
--- src/pages/Shared/Footer.jsx
+++ src/pages/Shared/Footer.jsx
@@ -1,29 +1,47 @@
-
+import { FaPhone, FaEnvelope, FaMapMarkerAlt, FaFacebook, FaYoutube, FaInstagram } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900">
-      <div className="container mx-auto px-6 py-8">
-        <div className="md:flex md:items-center md:justify-between">
-          <div className="flex-1 min-w-0">
-            <p className="text-gray-300 font-semibold">© 2023 Your Company. All rights reserved.</p>
-          </div>
-          <div className="mt-4 md:mt-0">
-            <nav className="flex">
-              <a href="#" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
-              <a href="#" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
-              <a href="#" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Services</a>
-              <a href="#" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
-            </nav>
-          </div>
+    <footer className="bg-gray-900 text-white py-10">
+    <div className="container mx-auto flex flex-col md:flex-row justify-between px-4">
+      <div className="flex flex-col items-center md:items-start">
+        <img src="/logo.png" alt="Website Logo" className="h-8 mb-4" />
+        <p className="text-sm">© 2023 Your Website. All rights reserved.</p>
+      </div>
+      <div className="flex flex-col items-center md:items-end">
+        <p className="text-sm mb-2">Contact Us:</p>
+        <ul className="flex space-x-4">
+          <li>
+            <a href="tel:1234567890" className="text-gray-300 hover:text-white transition duration-300">
+              <FaPhone className="inline-block mr-1" />
+              123-456-7890
+            </a>
+          </li>
+          <li>
+            <a href="mailto:info@yourwebsite.com" className="text-gray-300 hover:text-white transition duration-300">
+              <FaEnvelope className="inline-block mr-1" />
+              info@yourwebsite.com
+            </a>
+          </li>
+        </ul>
+        <p className="text-sm mt-4">
+          <FaMapMarkerAlt className="inline-block mr-1" />
+          1234 Main Street, City, State, ZIP
+        </p>
+        <div className="mt-4 flex">
+          <a href="https://www.facebook.com/yourpage" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-300 transition duration-300 mx-2">
+            <FaFacebook className="text-xl" />
+          </a>
+          <a href="https://www.youtube.com/yourchannel" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-300 transition duration-300 mx-2">
+            <FaYoutube className="text-xl" />
+          </a>
+          <a href="https://www.instagram.com/youraccount" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-300 transition duration-300 mx-2">
+            <FaInstagram className="text-xl" />
+          </a>
         </div>
       </div>
-      <div className="flex justify-center mt-4">
-        <a className="text-gray-300 hover:text-white mb-10 mx-2"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a>
-        <a className="text-gray-300 hover:text-white mx-2"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
-        <a className="text-gray-300 hover:text-white mx-2"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
-      </div>
-    </footer>
+    </div>
+  </footer>
   );
 };
 
