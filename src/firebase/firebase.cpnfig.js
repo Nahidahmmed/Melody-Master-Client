332149b970e11aa21aea3ceaee760c5c48807a61
Nahidahmmed <nahidahmmed411@gmail.com>
2024-02-09 19:57:10 +0600

    firebase setup

--- src/firebase/firebase.cpnfig.js
+++ src/firebase/firebase.cpnfig.js
@@ -5,14 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId
+  apiKey: "AIzaSyCo2X23vm2fRVt11m6FS8fv1smLKhVcCoU",
+  authDomain: "tempo-tutors-3d4b3.firebaseapp.com",
+  projectId: "tempo-tutors-3d4b3",
+  storageBucket: "tempo-tutors-3d4b3.appspot.com",
+  messagingSenderId: "447466441253",
+  appId: "1:447466441253:web:6d651469431ad464aa8dcc"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default app
